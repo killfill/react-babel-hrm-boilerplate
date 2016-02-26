f45f28720605180b1dd2893b05a20b68e2f06034
Phillip Neumann <pneumann@gmail.com>
2016-02-26 00:20:39 -0300

    better tip

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,19 @@ const Sub = (props) => {
 	return <h4>---------------- {props.texto}</h4>
 } 
 
+var tip = ["// ./store.js",
+"import { createStore } from 'redux'",
+"import reducer from './reducers'",
+"",
+"const setupStore = (initialState) => {",
+"	const store = createStore(reducer, initialState)",
+"	if (module.hot) { //HMR!",
+"		module.hot.accept('./reducers', _ => store.replaceReducer(require('./reducers').default))",
+"	}",
+"	return store",
+"}",
+"export default setupStore"].join('\n')
+
 class App extends React.Component {
 	constructor(props) {
 		super(props)
@@ -27,14 +40,7 @@ class App extends React.Component {
 			<h1>Cachate</h1>
 			<Sub texto={this.state.num}/>
 			<b>If your trying to use redux store, there is a trick to be able to hotload its reducers, like <a href='https://github.com/reactjs/redux/blob/master/examples/todomvc/store/configureStore.js'>this</a></b>
-			<pre>
-				  if (module.hot)
-				    module.hot.accept('./modules/reducer', () => 
-				      store.replaceReducer(require('./modules/reducer'));
-				    );
-				  
-			</pre>
-
+			<pre>{tip}</pre>
 		</div>
 		// return <h1>Sip {this.state.num + 100}</h1>
 	}
